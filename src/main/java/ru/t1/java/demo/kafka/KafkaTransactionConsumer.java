2ea9436a89252006cd7191c222f144c3a8538bb5
Vladyak1 <vkysnoteevo1@mail.ru>
2024-11-19 14:29:56 +0300

    Домашка 2

---
+++ src/main/java/ru/t1/java/demo/kafka/KafkaTransactionConsumer.java
@@ -0,0 +1,27 @@
+package ru.t1.java.demo.kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import ru.t1.java.demo.model.Transaction;
+import ru.t1.java.demo.repository.TransactionRepository;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class KafkaTransactionConsumer {
+
+    private final TransactionRepository transactionRepository;
+
+    @KafkaListener(topics = "t1_demo_transactions")
+    public void listener(Transaction transaction) {
+        try {
+            log.info("KafkaListener получил Transaction: {}", transaction);
+            transactionRepository.save(transaction);
+            log.info("Transaction сохранен в БД");
+        } catch (Exception e) {
+            log.error("Ошибка сохранения Transaction: ", e);
+        }
+    }
+}
